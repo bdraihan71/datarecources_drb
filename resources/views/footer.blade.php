a23c2b5e57b8197d2a83056365839577bcc83aa5
Asif Arefin Bonny <bonny@techynaf.com>
2019-11-05 02:16:40 +0600

    Fix footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Sticky Footer -->
 <footer>
-    <div class="text-center mt-5">
+    <div class="text-center mt-5 copyright w-100 py-3">
         <span>Copyright © Data Resource BD 2019</span>
     </div>
 </footer>
