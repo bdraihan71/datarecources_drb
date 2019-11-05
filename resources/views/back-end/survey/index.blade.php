e115e3ac3ff6eaeae82a0ae611b0c366ac2b368b
Asif Arefin Bonny <bonny@techynaf.com>
2019-11-05 22:07:08 +0600

    Fix index and edit header of survey

--- resources/views/back-end/survey/index.blade.php
+++ resources/views/back-end/survey/index.blade.php
@@ -4,8 +4,8 @@
 
 <form  method="post" action="{{ route('survey.store') }}">
     @csrf
-    <div class="row">
-        <div class="col-md-4">
+    <div class="row bg-white my-4 mx-1 p-3 shadow-sm">
+        <div class="col-md-3">
             <div class="form-group">
             <label>Survey Name</label>
             <input class="form-control" name="title"  value="{{ old('title') }}" type="text" placeholder="Enter Survey Name">
@@ -15,27 +15,25 @@
         <div class="col-md-4">
             <div class="form-group">
             <label>Survey Description</label>
-            <textarea class="form-control" rows="3" name="description" placeholder="Enter page description">{{ old('description') }}</textarea>
+            <textarea class="form-control" rows="1" name="description" placeholder="Enter page description">{{ old('description') }}</textarea>
             </div>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-3 mt-md-4 ml-4 ml-md-0 text-md-center">
             <input class="form-check-input" type="checkbox" name="is_published" value=1 id="defaultCheck2">
-            <label class="form-check-label" for="defaultCheck2">
+            <label class="form-check-label h5" for="defaultCheck2">
                 Publish
             </label>
-        </div>
-
-        <div class="col-md-4">
+            <br>
             <input class="form-check-input" type="checkbox" name="is_accepting_answer" value=1 id="defaultCheck1">
-            <label class="form-check-label" for="defaultCheck1">
+            <label class="form-check-label h5" for="defaultCheck1">
                 Accepting Answer
             </label>
         </div>
 
         <div class="col-md-2">
-            <label>.</label>
-            <button type="submit" class="btn btn-outline-primary w-100">Submit</button>
+            <label></label>
+            <button type="submit" class="btn btn-outline-primary w-100 mt-md-2">Submit</button>
         </div>
     </div>
 </form>
