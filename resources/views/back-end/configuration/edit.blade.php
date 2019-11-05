0f502321221b4bef01bd0f627f2a6c68d970e0a9
Asif Arefin Bonny <bonny@techynaf.com>
2019-11-05 22:19:05 +0600

    Fix edit header of configuration

--- resources/views/back-end/configuration/edit.blade.php
+++ resources/views/back-end/configuration/edit.blade.php
@@ -5,16 +5,16 @@
 <form  method="post" action="{{ route('configuration.update', $keyvaluepair->id) }}">
     @csrf
     @method('patch')
-    <div class="row">
-        <div class="col-md-4">
+    <div class="row bg-white my-4 mx-1 p-3 shadow-sm">
+        <div class="col-md-10">
             <div class="form-group">
             <label>{{ $keyvaluepair->key }}</label>
             <input class="form-control" name="value"  value="{{ $keyvaluepair->value }}" type="text" placeholder="Enter value">
             </div>
         </div>
         <div class="col-md-2">
-            <label>.</label>
-            <button type="submit" class="btn btn-outline-primary w-100">Update</button>
+            <label></label>
+            <button type="submit" class="btn btn-outline-primary w-100 mt-md-2">Update</button>
         </div>
     </div>
 </form>
