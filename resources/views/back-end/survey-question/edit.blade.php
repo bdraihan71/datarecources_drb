7f75e9d4d0ee6893d7edf2fb44d2575cdbe3662d
Asif Arefin Bonny <bonny@techynaf.com>
2019-11-05 22:16:17 +0600

    Fix index and edit header of survey question

--- resources/views/back-end/survey-question/edit.blade.php
+++ resources/views/back-end/survey-question/edit.blade.php
@@ -5,34 +5,30 @@
 <form  method="post" action="{{ route('surveyquestion.update', $surveyquestion->id) }}">
     @csrf
     @method('patch')
-    <div class="row">
-        <div class="col-md-4">
-            <div class="form-group ">
-                <label class="col-12 col-form-label">Survey:<span class="text-danger">*</span> </label>
-                <div class="col-12">
-                    <select class="form-control dropdown-custom" name="survey_id" require>
-                        @foreach($surveys as $survey)
-                            @if (($surveyquestion->survey->id) == $survey->id))
-                                <option value="{{$survey->id}}" selected>{{$survey->title}}</option>
-                            @else
-                                <option value="{{$survey->id}}">{{$survey->title}}</option>
-                            @endif
-                        @endforeach
-                    </select>
-                </div>
-            </div>
+    <div class="row bg-white my-4 mx-1 p-3 shadow-sm">
+        <div class="col-md-5">
+            <label>Survey:<span class="text-danger">*</span> </label>
+            <select class="form-control dropdown-custom" name="survey_id" require>
+                @foreach($surveys as $survey)
+                    @if (($surveyquestion->survey->id) == $survey->id))
+                        <option value="{{$survey->id}}" selected>{{$survey->title}}</option>
+                    @else
+                        <option value="{{$survey->id}}">{{$survey->title}}</option>
+                    @endif
+                @endforeach
+            </select>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-5">
             <div class="form-group">
-            <label>Question</label>
-            <input class="form-control" name="question"  value="{{ $surveyquestion->question }}" type="text" placeholder="Enter question">
+                <label>Question</label>
+                <input class="form-control" name="question"  value="{{ $surveyquestion->question }}" type="text" placeholder="Enter question">
             </div>
         </div>
 
         <div class="col-md-2">
-            <label>.</label>
-            <button type="submit" class="btn btn-outline-primary w-100">Update</button>
+            <label></label>
+            <button type="submit" class="btn btn-outline-primary w-100 mt-md-2">Update</button>
         </div>
     </div>
 </form>
