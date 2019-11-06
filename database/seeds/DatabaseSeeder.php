5a68db9138bf1cfaed57a89c5d749b97e94902e4
Gulshan Jubaed Prince <prince@techynaf.com>
2019-11-06 18:21:40 +0600

    Remove seeder call

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,19 +12,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        factory(App\Menu::class, 20)->create();
-        factory(App\Page::class, 20)->create();
-        factory(App\PageItem::class, 20)->create();
-        factory(App\Company::class, 20)->create();
-        factory(App\Announcment::class, 20)->create();
-        factory(App\Survey::class, 20)->create();
-        factory(App\SurveyQuestion::class, 20)->create();
-        factory(App\SurveyAnswerOption::class, 20)->create();
-        factory(App\SurveyHit::class, 20)->create();
-        factory(App\Transaction::class, 20)->create();
-        factory(App\StaticContent::class, 20)->create();
-        factory(App\Search::class, 20)->create();
-        factory(App\Sector::class, 20)->create();
-        factory(App\SubscriptionPlan::class, 20)->create();
+        // factory(App\Menu::class, 20)->create();
+        // factory(App\Page::class, 20)->create();
+        // factory(App\PageItem::class, 20)->create();
+        // factory(App\Company::class, 20)->create();
+        // factory(App\Announcment::class, 20)->create();
+        // factory(App\Survey::class, 20)->create();
+        // factory(App\SurveyQuestion::class, 20)->create();
+        // factory(App\SurveyAnswerOption::class, 20)->create();
+        // factory(App\SurveyHit::class, 20)->create();
+        // factory(App\Transaction::class, 20)->create();
+        // factory(App\StaticContent::class, 20)->create();
+        // factory(App\Search::class, 20)->create();
+        // factory(App\Sector::class, 20)->create();
+        // factory(App\SubscriptionPlan::class, 20)->create();
     }
 }
