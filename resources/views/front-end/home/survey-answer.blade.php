54a3faeb8a729aced44dea189da6ad8531f6ecdb
Asif Arefin Bonny <bonny@techynaf.com>
2019-11-27 20:11:50 +0600

    Remove stripes from progressbar

--- resources/views/front-end/home/survey-answer.blade.php
+++ resources/views/front-end/home/survey-answer.blade.php
@@ -13,7 +13,7 @@
                     </div>
                     <div class="col-md-10">
                         <div class="progress progress-height my-2">
-                            <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: {{ $surveyAnswerOption->hit_percent }}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar bg-warning" role="progressbar" style="width: {{ $surveyAnswerOption->hit_percent }}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                                 <label class="h5 pt-2" for="{{$surveyQuestion->id}}"> {{ $surveyAnswerOption->hit_count ? $surveyAnswerOption->hit_count : 0 }}</label>
                             </div>
                         </div>
