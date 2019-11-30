c54b1fb042977e53a097792a760d4e7b5e2c88b1
Asif Arefin Bonny <bonny@techynaf.com>
2019-11-30 20:47:53 +0600

    Fix label in page edit view

--- resources/views/back-end/page/edit.blade.php
+++ resources/views/back-end/page/edit.blade.php
@@ -8,14 +8,14 @@
     <div class="row bg-white my-4 mx-1 p-3 shadow-sm">
         <div class="col-md-4">
             <div class="form-group">
-            <label>Menu Name</label>
+            <label>Page Name</label>
             <input class="form-control" name="title"  value="{{ $page->title }}" type="text" placeholder="Enter Menu Name">
             </div>
         </div>
 
         <div class="col-md-4">
             <div class="form-group ">
-                <label>Parent Menu:<span class="text-danger">*</span> </label>
+                <label>Parent Page:<span class="text-danger">*</span> </label>
                 <select class="form-control dropdown-custom" name="menu_id" require>
                     @foreach($menus as $menu)
                         @if (($page->menu->id) == $menu->id))
