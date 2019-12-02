fbb247538d3f0d1b53d6d522d1eb53a64911ed5e
bdraihan71 <raihan@techynaf.com>
2019-12-02 14:40:36 +0600

    Fix Quarterly for search

--- resources/views/front-end/finance-info/quarterlydata.blade.php
+++ resources/views/front-end/finance-info/quarterlydata.blade.php
@@ -9,22 +9,33 @@
                 <th>Company</th>
                 <th>Ticker</th>
                 <th>Year</th>
-                @isset($q1)
+                @if ($frequency == 'quarterly' and $q1 == null and $q2 == null and $q3 == null and $q4 == null)
                     <th>Q1 PDF</th>
                     <th>Q1 Excel</th>
-                @endisset
-                @isset($q2)
                     <th>Q2 PDF</th>
                     <th>Q2 Excel</th>
-                @endisset
-                @isset($q3)
                     <th>Q3 PDF</th>
                     <th>Q3 Excel</th>
-                @endisset
-                @isset($q4)
                     <th>Q4 PDF</th>
                     <th>Q4 Excel</th>
-                @endisset
+                @else    
+                    @isset($q1)
+                        <th>Q1 PDF</th>
+                        <th>Q1 Excel</th>
+                    @endisset
+                    @isset($q2)
+                        <th>Q2 PDF</th>
+                        <th>Q2 Excel</th>
+                    @endisset
+                    @isset($q3)
+                        <th>Q3 PDF</th>
+                        <th>Q3 Excel</th>
+                    @endisset
+                    @isset($q4)
+                        <th>Q4 PDF</th>
+                        <th>Q4 Excel</th>
+                    @endisset
+                @endif    
 
             </tr>
             </thead>
@@ -33,22 +44,33 @@
                 <th>Company</th>
                 <th>Ticker</th>
                 <th>Year</th>
-                @isset($q1)
+                @if ($frequency == 'quarterly' and $q1 == null and $q2 == null and $q3 == null and $q4 == null)
                     <th>Q1 PDF</th>
                     <th>Q1 Excel</th>
-                @endisset
-                @isset($q2)
                     <th>Q2 PDF</th>
                     <th>Q2 Excel</th>
-                @endisset
-                @isset($q3)
                     <th>Q3 PDF</th>
                     <th>Q3 Excel</th>
-                @endisset
-                @isset($q4)
                     <th>Q4 PDF</th>
                     <th>Q4 Excel</th>
-                @endisset
+                @else    
+                    @isset($q1)
+                        <th>Q1 PDF</th>
+                        <th>Q1 Excel</th>
+                    @endisset
+                    @isset($q2)
+                        <th>Q2 PDF</th>
+                        <th>Q2 Excel</th>
+                    @endisset
+                    @isset($q3)
+                        <th>Q3 PDF</th>
+                        <th>Q3 Excel</th>
+                    @endisset
+                    @isset($q4)
+                        <th>Q4 PDF</th>
+                        <th>Q4 Excel</th>
+                    @endisset
+                @endif
             </tr>
             </tfoot>
             <tbody>
@@ -61,9 +83,9 @@
                     <td>{{$item->company->name}}</td>
                     <td>{{$item->company->ticker}}</td>
                     <td>{{$item->year}}</td>
-                    @isset($q1)
+                    @if ($frequency == 'quarterly' and $q1 == null and $q2 == null and $q3 == null and $q4 == null)
                         @if($item->q1__pdf_url != '#')
-                            <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q1__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q1__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
                         @else
                             <td>No PDF</td>
                         @endif
@@ -72,11 +94,9 @@
                         @else
                             <td>No Excel</td>
                         @endif
-                    @endisset
 
-                    @isset($q2)
                         @if($item->q2__pdf_url != '#')
-                            <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q2__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q2__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
                         @else
                             <td>No PDF</td>
                         @endif
@@ -85,10 +105,7 @@
                         @else
                             <td>No Excel</td>
                         @endif
-                    @endisset
 
-                    
-                    @isset($q3)
                         @if($item->q3__pdf_url != '#')
                             <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q3__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
                         @else
@@ -99,12 +116,9 @@
                         @else
                             <td>No Excel</td>
                         @endif
-                    @endisset
 
-
-                    @isset($q4)
                         @if($item->q4__pdf_url != '#')
-                            <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q4__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q4__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
                         @else
                             <td>No PDF</td>
                         @endif
@@ -113,7 +127,61 @@
                         @else
                             <td>No Excel</td>
                         @endif
-                    @endisset    
+                    @else  
+                        @isset($q1)
+                            @if($item->q1__pdf_url != '#')
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q1__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
+                            @else
+                                <td>No PDF</td>
+                            @endif
+                            @if($item->q1_excel_url != '#')
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q1_excel_url }}" type="button" class="btn btn-outline-primary">Excel</a></td>
+                            @else
+                                <td>No Excel</td>
+                            @endif
+                        @endisset
+
+                        @isset($q2)
+                            @if($item->q2__pdf_url != '#')
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q2__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
+                            @else
+                                <td>No PDF</td>
+                            @endif
+                            @if($item->q2_excel_url != '#')
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q2_excel_url }}" type="button" class="btn btn-outline-primary">Excel</a></td>
+                            @else
+                                <td>No Excel</td>
+                            @endif
+                        @endisset
+
+                        
+                        @isset($q3)
+                            @if($item->q3__pdf_url != '#')
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q3__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
+                            @else
+                                <td>No PDF</td>
+                            @endif
+                            @if($item->q3_excel_url != '#')
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q3_excel_url }}" type="button" class="btn btn-outline-primary">Excel</a></td>
+                            @else
+                                <td>No Excel</td>
+                            @endif
+                        @endisset
+
+
+                        @isset($q4)
+                            @if($item->q4__pdf_url != '#')
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q4__pdf_url }}" type="button" class="btn btn-outline-primary">PDF</a></td>
+                            @else
+                                <td>No PDF</td>
+                            @endif
+                            @if($item->q4_excel_url != '#')
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->q4_excel_url }}" type="button" class="btn btn-outline-primary">Excel</a></td>
+                            @else
+                                <td>No Excel</td>
+                            @endif
+                        @endisset
+                    @endif        
                 </tr>
             @endforeach
             @else
