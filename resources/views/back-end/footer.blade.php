00b05dd91e778ab99dc63327c7c0bb79aa0a22cb
Asif Arefin Bonny <bonny@techynaf.com>
2019-12-04 17:48:14 +0600

    Fix resource to resources in footer

--- resources/views/back-end/footer.blade.php
+++ resources/views/back-end/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="mt-5">
     <div class="text-center mt-5 copyright w-100 py-3">
         <span>
-            <a href="https://www.techynaf.com" target="_blank">Copyright &copy; Data Resource Bangladesh 2019 
+            <a href="https://www.techynaf.com" target="_blank">Copyright &copy; Data Resources Bangladesh 2019 
                 <br><span class="small">Made with <span class="text-danger">&hearts;</span> by Techynaf Technologies Limited</span>
             </a>
         </span>
