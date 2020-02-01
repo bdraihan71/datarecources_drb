27578b80294312bf926e47f13b9d97b90f798464
unknown <raihan@techynaf.com>
2020-02-01 14:27:49 +0600

    Fix subscribe index blade

--- resources/views/back-end/subscription-plan/index.blade.php
+++ resources/views/back-end/subscription-plan/index.blade.php
@@ -5,7 +5,7 @@
 <form  method="post" action="{{ route('subscriptionplan.store') }}">
     @csrf
     <div class="row bg-white my-4 mx-1 p-3 shadow-sm">
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="form-group">
             <label>Plan Name</label>
             <input class="form-control" name="name"  value="{{ old('name') }}" type="text" placeholder="Enter Plan Name">
@@ -14,19 +14,26 @@
 
         <div class="col-md-2">
             <div class="form-group">
-            <label>Price </label>
-            <input class="form-control" name="price"  value="{{ old('price') }}" type="number" placeholder="Enter Price">
+            <label>Price Per Month </label>
+            <input class="form-control" name="price_per_month"  value="{{ old('price_per_month') }}" type="number" placeholder="Enter Monthly Price">
             </div>
         </div>
 
         <div class="col-md-2">
             <div class="form-group">
-            <label>Duration</label>
-            <input class="form-control" name="duration_in_days"  value="{{ old('duration_in_days') }}" type="number" placeholder="Enter Duration">
+            <label>Price Per Year</label>
+            <input class="form-control" name="price_per_year"  value="{{ old('price_per_year') }}" type="number" placeholder="Enter yearly Price">
             </div>
         </div>
 
-        <div class="col-md-2 text-md-center ml-4 ml-md-0 sub-plan-check-top">
+        <div class="col-md-2">
+            <div class="form-group">
+            <label>User Limit</label>
+            <input class="form-control" name="user_limit"  value="{{ old('user_limit') }}" type="number" placeholder="Enter User Limit">
+            </div>
+        </div>
+
+        <div class="col-md-1 text-md-center ml-4 ml-md-0 sub-plan-check-top">
             <input class="form-check-input" type="checkbox" name="is_visible" value=1 id="defaultCheck1">
             <label class="form-check-label h5" for="defaultCheck1">
                 Visible
