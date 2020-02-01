e7675142ec7866896394b9bb810737dfa25f31d5
unknown <raihan@techynaf.com>
2020-02-01 15:15:09 +0600

    Add discount method

--- app/DrbModel.php
+++ app/DrbModel.php
@@ -10,4 +10,12 @@ class DrbModel extends Model
     use SoftDeletes;
     protected $guarded = [];
     protected $dates = ['deleted_at'];
+
+    public function discount( $monthly, $yearly )
+    {
+        $discount = (($monthly - ($yearly / 12)) * 100) / $monthly;
+
+        return $discount;
+        
+    }
 }
