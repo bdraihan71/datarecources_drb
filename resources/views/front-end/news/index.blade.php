cc75dc7b3208eb88e7fb4cefcd727a7cd8d60c5f
Asif Arefin Bonny <bonny@techynaf.com>
2020-02-07 19:13:33 +0600

    Fix news index

--- resources/views/front-end/news/index.blade.php
+++ resources/views/front-end/news/index.blade.php
@@ -3,13 +3,13 @@
 <!-- Navigation -->
 
 <section class="news">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row custom-header-top">
             <div class="col-md-12 mt-5">
                 <h3>News</h3>
             </div>
-            <div class="col-md-2"></div>
-            <div class="col-md-8 mb-5">
+            <div class="col-md-3"></div>
+            <div class="col-md-6 mb-5">
                 <form action="{{route('search')}}" method="GET">
                     <div class="input-group mt-2">
                         <input class="form-control border-secondary search-border border border-secondary" type="search" value="" name="search" placeholder=" Search by keyword">
@@ -21,32 +21,23 @@
                     </div>
                 </form>
             </div>
-            <div class="col-md-2"></div>
+            <div class="col-md-3"></div>
             <div class="col-md-12">
-                <ul class="list-unstyled">
-                    @foreach($allnews as $news)
-                        <a href="{{route('news.single',$news->id)}}">
-                            <li class="media">
-                            <img src="{{ env('S3_URL') }}{{$news->image}}" class="mr-3 img-fluid news-list-img" alt="...">
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">{{ $news->heading }}</h5>
-                                    {{ implode(' ', array_slice(explode(' ', $news->body ), 0, 70))}}
-                                <br>
-                                <a href="{{route('news.single',$news->id)}}">See More ></a>
-                                <br>
-                                {{-- <div class="text-right">
-                                    <h6>Share</h6>
-                                    <div class="addthis_inline_share_toolbox mx-auto"></div>
-                                    <i class="fab fa-facebook-square fa-2x"></i>
-                                    <i class="fab fa-twitter-square fa-2x"></i>
-                                    <i class="fab fa-linkedin fa-2x"></i>
-                                </div> --}}
-                            </div>
-                            </li>
-                        </a>
-                        <hr>
-                    @endforeach
-                </ul>
+                @foreach($allnews as $news)
+                    <div class="row">
+                        <div class="col-md-2">
+                            <a href="{{route('news.single',$news->id)}}">
+                                <img src="{{ env('S3_URL') }}{{$news->image}}" class="mr-3 img-fluid" alt="...">
+                            </a>
+                        </div>
+                        <div class="col-md-10">
+                            <a href="{{route('news.single',$news->id)}}"><h5>{{ $news->heading }}</h5></a>
+                            <a href="{{route('news.single',$news->id)}}"><p>{{ implode(' ', array_slice(explode(' ', $news->body ), 0, 70))}}</p></a>
+                            <a href="{{route('news.single',$news->id)}}">See More ></a>
+                        </div>
+                    </div>
+                    <hr>
+                @endforeach
             </div>
             <div class="col-md-12 my-5">
                 <nav aria-label="Page navigation example">
