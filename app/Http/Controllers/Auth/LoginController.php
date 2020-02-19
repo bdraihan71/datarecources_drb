79404570fae9d7f836600a2b331acf62170f4d0e
bdraihan71 <raihan@techynaf.com>
2020-02-19 19:30:06 +0600

    Fix login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Subscriber;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\StaticContent;
@@ -37,6 +42,41 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        $user = User::where('email', $request->email)->latest()->first();
+        $subscriber = Subscriber::where('user_id', $user->id)->latest()->first();
+
+        if($subscriber->expiry_date <= date("Y-m-d")){
+            $subscriber = Subscriber::find($subscriber->id);
+            $subscriber->is_expire = true;
+            $subscriber->save();
+        }
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
 
     public function showLoginForm ()
     {
