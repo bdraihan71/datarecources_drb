270e50f355e6a66b0fa9b624de17998a41f29796
bdraihan71 <raihan@techynaf.com>
2020-02-19 20:40:53 +0600

    Fix finalcial page

--- resources/views/front-end/finance-info/datatable.blade.php
+++ resources/views/front-end/finance-info/datatable.blade.php
@@ -142,85 +142,109 @@
                     @endif 
                     
                     @if(auth()->user())
-                        @if($user->subscribers->last()->is_expire != null)
-                           <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
-                        @else
-                            @if($item->annual_excel_url != '#')
-                            <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_excel_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_excel_url_file_name != null ? $item->annual_excel_url_file_name : 'Excel' }}</a></td>
+                        @if($user->subscribers->last() != null)
+                            @if($user->subscribers->last()->is_expire != null)
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
                             @else
-                                <td>No Excel</td>
+                                @if($item->annual_excel_url != '#')
+                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_excel_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_excel_url_file_name != null ? $item->annual_excel_url_file_name : 'Excel' }}</a></td>
+                                @else
+                                    <td>No Excel</td>
+                                @endif
                             @endif
+                        @else
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
                         @endif    
                     @else
                         <td><a href="/login" class="btn btn-warning">Login</a></td>   
                     @endif  
 
                     @if(auth()->user())
-                        @if($user->subscribers->last()->is_expire != null)
-                           <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
-                        @else
-                            @if($item->annual_pdf_1_url != '#')
-                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_1_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_1_url_file_name != null ? $item->annual_pdf_1_url_file_name : 'PDF' }}</a></td>
+                        @if($user->subscribers->last() != null)
+                            @if($user->subscribers->last()->is_expire != null)
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
                             @else
-                                <td>No PDF</td>
+                                @if($item->annual_pdf_1_url != '#')
+                                    <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_1_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_1_url_file_name != null ? $item->annual_pdf_1_url_file_name : 'PDF' }}</a></td>
+                                @else
+                                    <td>No PDF</td>
+                                @endif
                             @endif
+                        @else
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
                         @endif    
                     @else
                         <td><a href="/login" class="btn btn-warning">Login</a></td>   
                     @endif 
 
                     @if(auth()->user())
-                        @if($user->subscribers->last()->is_expire != null)
-                           <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
-                        @else
-                            @if($item->annual_pdf_2_url != '#')
-                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_2_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_2_url_file_name != null ? $item->annual_pdf_2_url_file_name : 'PDF' }}</a></td>
+                        @if($user->subscribers->last() != null)
+                            @if($user->subscribers->last()->is_expire != null)
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
                             @else
-                                <td>No PDF</td>
+                                @if($item->annual_pdf_2_url != '#')
+                                    <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_2_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_2_url_file_name != null ? $item->annual_pdf_2_url_file_name : 'PDF' }}</a></td>
+                                @else
+                                    <td>No PDF</td>
+                                @endif
                             @endif
-                        @endif    
+                        @else
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
+                        @endif         
                     @else
                         <td><a href="/login" class="btn btn-warning">Login</a></td>   
                     @endif 
 
                     @if(auth()->user())
-                        @if($user->subscribers->last()->is_expire != null)
-                           <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
-                        @else
-                            @if($item->annual_pdf_3_url != '#')
-                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_3_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_3_url_file_name != null ? $item->annual_pdf_3_url_file_name : 'PDF' }}</a></td>
+                        @if($user->subscribers->last() != null)
+                            @if($user->subscribers->last()->is_expire != null)
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
                             @else
-                                <td>No PDF</td>
+                                @if($item->annual_pdf_3_url != '#')
+                                    <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_3_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_3_url_file_name != null ? $item->annual_pdf_3_url_file_name : 'PDF' }}</a></td>
+                                @else
+                                    <td>No PDF</td>
+                                @endif
                             @endif
-                        @endif    
+                        @else
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
+                        @endif         
                     @else
                         <td><a href="/login" class="btn btn-warning">Login</a></td>   
                     @endif 
 
                     @if(auth()->user())
-                        @if($user->subscribers->last()->is_expire != null)
-                           <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
-                        @else
-                            @if($item->annual_pdf_4_url != '#')
-                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_4_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_4_url_file_name != null ? $item->annual_pdf_4_url_file_name : 'PDF' }}</a></td>
+                        @if($user->subscribers->last() != null)
+                            @if($user->subscribers->last()->is_expire != null)
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
                             @else
-                                <td>No PDF</td>
+                                @if($item->annual_pdf_4_url != '#')
+                                    <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_4_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_4_url_file_name != null ? $item->annual_pdf_4_url_file_name : 'PDF' }}</a></td>
+                                @else
+                                    <td>No PDF</td>
+                                @endif
                             @endif
-                        @endif    
+                        @else
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
+                        @endif      
                     @else
                         <td><a href="/login" class="btn btn-warning">Login</a></td>   
                     @endif 
 
                     @if(auth()->user())
-                        @if($user->subscribers->last()->is_expire != null)
-                           <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
-                        @else
-                            @if($item->annual_pdf_5_url != '#')
-                                <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_5_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_5_url_file_name != null ? $item->annual_pdf_5_url_file_name : 'PDF' }}</a></td>
+                        @if($user->subscribers->last() != null)
+                            @if($user->subscribers->last()->is_expire != null)
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
                             @else
-                                <td>No PDF</td>
+                                @if($item->annual_pdf_5_url != '#')
+                                    <td><a target="_blank" href="{{ env('S3_URL') }}{{ $item->annual_pdf_5_url }}" type="button" class="btn btn-outline-primary">{{ $item->annual_pdf_5_url_file_name != null ? $item->annual_pdf_5_url_file_name : 'PDF' }}</a></td>
+                                @else
+                                    <td>No PDF</td>
+                                @endif
                             @endif
-                        @endif    
+                        @else
+                            <td><a href="#" class="btn btn-warning">Please Subscribe</a></td>
+                        @endif      
                     @else
                         <td><a href="/login" class="btn btn-warning">Login</a></td>   
                     @endif 
