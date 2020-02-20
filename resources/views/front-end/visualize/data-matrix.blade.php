79de53237618d30802b0ab54eee539abfb5f3e38
Asif Arefin Bonny <bonny@techynaf.com>
2020-02-20 19:37:23 +0600

    Fix data matrix design

--- resources/views/front-end/visualize/data-matrix.blade.php
+++ resources/views/front-end/visualize/data-matrix.blade.php
@@ -1,7 +1,13 @@
 @extends('front-end.main-layout')
 
 @section('content')
-  @foreach($sectors as $sector)
-    @include('front-end.visualize.sector-datatable', compact('sector'))
-  @endforeach
+<div class="container-fluid">
+  <div class="row custom-header-top">
+      <div class="col-md-12 mt-5">
+        @foreach($sectors as $sector)
+          @include('front-end.visualize.sector-datatable', compact('sector'))
+        @endforeach
+    </div>
+  </div>
+</div>
 @endsection
