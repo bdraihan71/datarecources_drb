29698742ef3370ef56637ff74aa543db1ebe8c21
Asif Arefin Bonny <bonny@techynaf.com>
2020-05-05 20:40:38 +0600

    Fix news category page design

--- resources/views/front-end/news/continuous-index-category.blade.php
+++ resources/views/front-end/news/continuous-index-category.blade.php
@@ -11,7 +11,7 @@
                     <span>Data Resource BD</span>
                 </button>
             </div>
-            <div class="col-md-2">
+            <div class="col-12 col-md-2">
                 <div class="wrapper">
                     <!-- Sidebar  -->
                     <nav id="sidebar" class="bg-transparent text-dark custom-news-nav-header-top news-sidenav-scroll-hide">
@@ -29,12 +29,12 @@
                     </nav>
                 </div>
             </div>
-            <div class="col-md-7">
+            <div class="col-12 col-md-7">
                 
 
                 <!-- ///////////////////////////////////////////////// -->
 
-                <ul id="example-1">
+                <ul id="example-1" style="margin: 0; padding: 0;">
                     <div v-for="item in initial" :key="item.id">
                     <div class="shadow-sm mb-3 single-news-border">
                         <div class="row" v-bind:id="item.id">
@@ -71,7 +71,7 @@
                                         </div>
                                         <div class="col-4 col-md-2">
                                             <input type="hidden" name="news_id" v-bind:value="item.id">
-                                            <button type="submit" class="btn btn-warning w-100 float-right">Submit</button>
+                                            <button type="submit" class="btn btn-warning w-100 float-right"><i class="fas fa-check d-xl-none"></i> <span class="d-none d-xl-block">Submit</span></button>
                                         </div>
                                     </div>
                                 </form>
@@ -86,34 +86,39 @@
                                                 @csrf
                                                 @method('patch')
                                                 <div class="row">
-                                                    <div class="col-10">
+                                                    <div class="col-9 col-md-10">
                                                         <div class="form-group">
                                                             <textarea class="form-control mr-5" id="exampleFormControlTextarea1" name="body" rows="1" placeholder="Write a comment..." :value="comment.body"></textarea>
                                                         </div>
                                                     </div>
-                                                    <div class="col-2">
+                                                    <div class="col-3 col-md-2">
                                                         <input type="hidden" name="news_id" v-bind:value="item.id">
-                                                        <button type="submit" class="btn btn-warning w-100 float-right">Update</button>
+                                                        <button type="submit" class="btn btn-warning w-100 float-right"><i class="fas fa-check d-xl-none"></i> <span class="d-none d-xl-block">Update</span></button>
                                                     </div>
                                                 </div>
                                             </form>
                                         </span> 
                                         <span v-else>@{{comment.body}}</span>
-                                        @if(Auth::user())
-                                            {{-- @if(Auth::user()->id == 1)  --}}
-                                            <diV v-if="userId == comment.user_id">
-                                                <button class="bg-transparent border-0 small text-secondary" @click="isComment(comment.id)"  v-if="isShowCommentBox != comment.id">edit</button>
-                                                <button class="bg-transparent border-0 small text-secondary" @click="isComment(null)"  v-if="isShowCommentBox == comment.id">Cancel</button>
-                                                <form :action="route + comment.id" onclick="return confirm('Are you sure, you want to delete this Comment?')" method="post" style="display: inline;" v-if="isShowCommentBox != comment.id">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button type="submit" class="bg-transparent border-0 small text-secondary">Delete</button>
-                                                </form>
-                                            </diV>
-                                            {{-- @endif --}}
-                                        @endif    
-                                        <br> 
-                                        <span class="text-secondary news-comment-time-text mt-n3">@{{comment.human_readable_time}}</span>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <span class="text-secondary news-comment-time-text mt-n3">@{{comment.human_readable_time}}</span>
+                                            </div>
+                                            <div class="col-md-6 text-right">
+                                                @if(Auth::user())
+                                                    {{-- @if(Auth::user()->id == 1)  --}}
+                                                    <diV v-if="userId == comment.user_id">
+                                                        <button class="bg-transparent border-0 small text-secondary" @click="isComment(comment.id)"  v-if="isShowCommentBox != comment.id">edit</button>
+                                                        <button class="bg-transparent border-0 small text-secondary" @click="isComment(null)"  v-if="isShowCommentBox == comment.id">Cancel</button>
+                                                        <form :action="route + comment.id" onclick="return confirm('Are you sure, you want to delete this Comment?')" method="post" style="display: inline;" v-if="isShowCommentBox != comment.id">
+                                                            @csrf
+                                                            @method('delete')
+                                                            <button type="submit" class="bg-transparent border-0 small text-secondary">Delete</button>
+                                                        </form>
+                                                    </diV>
+                                                    {{-- @endif --}}
+                                                @endif
+                                            </div>
+                                        </div>
                                     </li>
                                 {{-- @endforeach --}}
                                 </div>
@@ -124,7 +129,7 @@
                 </ul>
             </div>
                 <!-- ////////////////////////////////////////////////// -->
-            <div class="col-md-3">
+            <div class="col-12 col-md-3">
                 <h5>Most Recent</h5>
                 <div class="table-responsive most-recent-border mb-3">
                     <table class="table table-hover">
